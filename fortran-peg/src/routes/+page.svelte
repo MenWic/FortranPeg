f7c89174b8ed4c22738c1fd3d72b71da58d86dfd
OkKid-A <miguelqueme@outlook.com>
2024-12-08 21:59:39 -0600

    Se ha agregado color y funcionalidad a la vista.

--- fortran-peg/src/routes/+page.svelte
+++ fortran-peg/src/routes/+page.svelte
@@ -1,9 +1,14 @@
-<div class="p-5 flex flex-row h-full max-h-full">
-    <div class="h-full w-1/2 mx-2 rounded-md border-black border overflow-y-scroll">
-        <CodeMirror bind:value={editorText} on:change={parse}/>
+<header class="header">
+    <h1 class="header-title">FortranPeg</h1>
+</header>
+<div class="p-5 flex flex-col h-full max-h-full space-y-4">
+    <h2 class="subheader">Text Editor</h2>
+    <div class="h-2/3 rounded-md border border-black overflow-y-scroll">
+        <CodeMirror bind:value={editorText} on:change={parse} />
     </div>
-    <div class="rounded-md border-black border-2 h-full w-1/2 mx-2">
-        <p class="text-lime-950">
+    <h2 class="subheader">Resultado</h2>
+    <div class="h-1/3 rounded-md border border-black flex items-start justify-start p-4">
+        <p class="{result === 'Codigo fuente parseado, todo bien...' ? 'text-lime' : 'text-red'}">
             {result}
         </p>
     </div>
@@ -13,10 +18,41 @@
     import parseInput from "$lib/parser";
     import CodeMirror from "svelte-codemirror-editor";
 
-    let editorText = $state("");
-    let result = $state("");
-    
+    let editorText = "";
+    let result = "";
+
     function parse() {
         result = parseInput(editorText.trim());
     }
 </script>
+
+<style>
+    .header {
+        background-color: rgba(239, 227, 227, 0.5);
+        padding: 20px;
+        text-align: left;
+        border-radius: 0;
+    }
+
+    .header-title {
+        margin: 0;
+        font-size: 2.5rem;
+        font-family: 'Roboto', sans-serif;
+        color: #000000;
+    }
+
+    .subheader {
+        font-size: 1.5rem;
+        font-family: 'Roboto', sans-serif;
+        color: #333;
+        margin-bottom: 10px;
+    }
+
+    .text-lime {
+        color: #3a7c64;
+    }
+
+    .text-red {
+        color: red;
+    }
+</style>
